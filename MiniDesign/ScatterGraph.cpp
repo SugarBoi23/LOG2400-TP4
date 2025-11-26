27408de786dd35467bed15c9e972baea6e7263b3
Hien-Minh Tu <tuhienminh0@gmail.com>
2025-11-26 13:28:27 -0500

    Add scatter graph base class

--- MiniDesign/ScatterGraph.cpp
+++ MiniDesign/ScatterGraph.cpp
@@ -1,5 +1,30 @@
-//
-// Created by shminh on 2025-11-26.
-//
-
 #include "ScatterGraph.hpp"
+
+void ScatterGraph::fusion() {
+
+    // TODO
+}
+
+void ScatterGraph::showPoints() {
+    // TODO
+}
+
+void ScatterGraph::movePoint(const std::string& ID, const Point newPosition) {
+    // TODO
+}
+
+void ScatterGraph::listPoints() {
+    // TODO
+}
+
+void ScatterGraph::deletePoint(const std::string &ID) {
+    // TODO
+}
+
+void ScatterGraph::buildEdges() {
+    // TODO
+}
+
+void ScatterGraph::exit() {
+    // TODO
+}
--- MiniDesign/ScatterGraph.hpp
+++ MiniDesign/ScatterGraph.hpp
@@ -1,14 +1,40 @@
-//
-// Created by shminh on 2025-11-26.
-//
+#pragma once
 
-#ifndef LOG2400_TP4_SCATTERGRAPH_HPP
-#define LOG2400_TP4_SCATTERGRAPH_HPP
+#include "affichage.hpp"
+#include "Display/DisplayID.hpp"
+#include "Display/DisplayTexture.hpp"
+#include "EdgeBuilder/EdgeBuilderID.hpp"
+#include "EdgeBuilder/EdgeBuilderDistance.hpp"
 
+#include <map>
+#include <string>
 
 class ScatterGraph {
+public:
+    explicit ScatterGraph() = default;
+    ~ScatterGraph() = default;
 
+    // command a
+    void listPoints();
+    // command o1 & o2
+    void showPoints();
+    // command f
+    void fusion();
+    // command d
+    void movePoint(const std::string& ID, Point newPosition);
+    // command s
+    void deletePoint(const std::string& ID);
+    // command c1 & c2
+    void buildEdges();
+    // command q
+    void exit();
+
+
+private:
+    std::map<std::string, Point> points_;
+    std::string texture_;
+
+    Display display_;
+    EdgeBuilder edgeBuilder_;
 };
 
-
-#endif //LOG2400_TP4_SCATTERGRAPH_HPP
